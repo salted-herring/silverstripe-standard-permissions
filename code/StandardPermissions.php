f0b435957ce6478676273f48af6b78bf39aa5b03
Simon Winter <simon@saltedherring.com>
2018-04-11 17:09:22 +1200

    Altered Permissions to allow more readable permission names

--- code/StandardPermissions.php
+++ code/StandardPermissions.php
@@ -1,96 +1,81 @@
 <?php
-	/**
-	 * @file
-	 * Adds CMS defined permissions into the code.
-	 */
-	
-	/**
-	 * An extension which controls the 'can*' functions in an object.
-	 */
-	class StandardPermissions extends DataExtension implements PermissionProvider {
-	
-		/**
-		 * Can the user view this?
-		 */
-		public function canView($member = false) {
-			return Permission::check(get_class($this->owner) . '_view');
-		}
-	
-		/**
-		 * Can the user edit this?
-		 */
-		public function canEdit($member = false) {
-			return Permission::check(get_class($this->owner) . '_edit');
-		}
-	
-		/**
-		 * Can the user delete this?
-		 */
-		public function canDelete($member = false) {
-			return Permission::check(get_class($this->owner) . '_delete');
-		}
-	
-		/**
-		 * Can the user create this?
-		 */
-		public function canCreate($member = false) {
-			return Permission::check(get_class($this->owner) . '_create');
-		}
-		
-		/**
-		 * Can the user publish this?
-		 */
-		public function canPublish($member = false) {
-			return Permission::check(get_class($this->owner) . '_publish');
-		}
-	
-		/**
-		 * Get a complete list of all the permissions this class uses.
-		 */
-		public function providePermissions() {
-	
-			// Prepare variables.
-			$permissions = array();
-	
-			// For each class...
-			foreach ($this->getClassList() as $class) {
-	
-				// ...add a few permissions.
-				foreach (array(
-					'view', 'edit', 'delete', 'create', 'publish',
-				) as $name) {
-	
-					$permissions[$class . '_' . $name] = $class . '_' . $name;
-				}
-			}
-	
-			// Done.
-			return $permissions;
-		}
-	
-		/**
-		 * Get a list of classes which 'we' extend.
-		 */
-		private function getClassList() {
-	
-			// Prepare variables.
-			$classes = array();
-	
-			// Go through all the classes.
-			foreach (ClassInfo::subclassesFor('DataObject') as $class => $file) {
-	
-				// What are the extensions for this class?
-				$extensions = Config::inst()->get($class, 'extensions');
-				if (!is_null($extensions)) {
-	
-					// Get a list of classes this horizontally extends this.
-					if (in_array(get_class($this), $extensions)) {
-						$classes[] = $class;
-					}
-				}
-			}
-	
-			// Done.
-			return $classes;
-		}
-	}
+/**
+ * @file StandardPermissions.php
+ * Adds CMS defined permissions for publishing
+ */
+
+/**
+ * An extension which controls the 'can*' functions in an object.
+ */
+class StandardPermissions extends DataExtension implements PermissionProvider
+{
+
+    /**
+     * Can the user publish this?
+     */
+    public function canPublish($member = false)
+    {
+        return Permission::check(get_class($this->owner) . '_publish');
+    }
+
+    /**
+     * Can the user unpublish this?
+     */
+    public function canUnpublish($member = false)
+    {
+        return Permission::check(get_class($this->owner) . '_unpublish');
+    }
+
+    /**
+     * Can the user archive this?
+     */
+    public function canArchive($member = false)
+    {
+        return Permission::check(get_class($this->owner) . '_archive');
+    }
+
+    public function canView($member = false)
+    {
+        return Permission::check(get_class($this->owner) . '_view');
+    }
+    public function canEdit($member = false)
+    {
+        return Permission::check(get_class($this->owner) . '_edit');
+    }
+    public function canDelete($member = false)
+    {
+        return Permission::check(get_class($this->owner) . '_delete');
+    }
+    public function canCreate($member = false)
+    {
+        return Permission::check(get_class($this->owner) . '_create');
+    }
+
+    /**
+     * Get a complete list of all the permissions this class uses.
+     */
+    public function providePermissions()
+    {
+        $permissions = array();
+        $config = Config::inst()->get('PublishProvider', 'classes');
+
+        foreach ($config as $class) {
+            $subClasses = array_keys(ClassInfo::subclassesFor($class));
+            
+            foreach ($subClasses as $subClass) {
+                foreach (array(
+                        'publish',
+                        'unpublish',
+                        'archive',
+                        'view',
+                        'edit',
+                        'delete',
+                        'create') as $name) {
+                    $permissions[$subClass . '_' . $name] = singleton($subClass)->singular_name(). ' ' . $name;
+                }
+            }
+        }
+        
+        return $permissions;
+    }
+}
